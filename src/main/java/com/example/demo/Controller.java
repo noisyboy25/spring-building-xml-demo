4371caa75df59b22c7404201d70526fef78805ac
noisyboy25 <dj402and@gmail.com>
2021-07-08 00:01:56 +0300

    Add DELETE method

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -2,45 +2,103 @@ package com.example.demo;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import com.example.demo.model.Building;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-  @GetMapping(value = "/building", produces = "application/json")
-  public Building allBuildings() {
+  final File dbFile = new File("building.xml");
+
+  @GetMapping(value = "/building/{id}", produces = "application/json")
+  public Optional<Building> oneBuilding(@PathVariable String id) {
     var xmlMapper = new XmlMapper();
 
-    Building data = null;
+    List<Building> dataBase = new ArrayList<>();
+    Optional<Building> building = Optional.empty();
 
     try {
-      data = xmlMapper.readValue(new File("building.xml"), Building.class);
+      dataBase = xmlMapper.readValue(dbFile, new TypeReference<List<Building>>() {
+      });
+      building = dataBase.stream().filter(b -> b.getId().equals(id)).findFirst();
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
 
-    return data;
+    return building;
+  }
+
+  @GetMapping(value = "/building", produces = "application/json")
+  public List<Building> allBuildings() {
+    var xmlMapper = new XmlMapper();
+
+    List<Building> dataBase = new ArrayList<>();
+
+    try {
+      dataBase = (List<Building>) xmlMapper.readValue(dbFile, new TypeReference<List<Building>>() {
+      });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return dataBase;
   }
 
   @PostMapping(value = "/building", produces = "application/json")
   public Building addBuilding(@RequestBody Building newBuilding) {
+
     var xmlMapper = new XmlMapper();
 
+    List<Building> dataBase = new ArrayList<>();
+
     try {
-      xmlMapper.writeValue(new File("building.xml"), newBuilding);
+      dataBase = (List<Building>) xmlMapper.readValue(dbFile, new TypeReference<List<Building>>() {
+      });
     } catch (IOException e) {
-      // TODO Auto-generated catch block
       e.printStackTrace();
     }
 
+    dataBase.add(newBuilding);
+
+    try {
+      xmlMapper.writeValue(dbFile, dataBase);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     return newBuilding;
+  }
+
+  @DeleteMapping(value = "/building/{id}", produces = "application/json")
+  public void deleteBuilding(@PathVariable String id) {
+
+    var xmlMapper = new XmlMapper();
+
+    List<Building> dataBase = new ArrayList<>();
+
+    try {
+      dataBase = (List<Building>) xmlMapper.readValue(dbFile, new TypeReference<List<Building>>() {
+      });
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    dataBase.removeIf(b -> b.getId().equals(id));
+
+    try {
+      xmlMapper.writeValue(dbFile, dataBase);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
