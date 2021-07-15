4a37f38152d64c6ced687f9a1db7ff4cedcae1ca
noisyboy25 <dj402and@gmail.com>
2021-07-15 15:05:20 +0300

    Change file to XmlDatabase for JSON API

--- src/main/java/com/example/demo/controller/BuildingController.java
+++ src/main/java/com/example/demo/controller/BuildingController.java
@@ -1,14 +1,14 @@
 package com.example.demo.controller;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
+import com.example.demo.lib.XmlDatabase;
 import com.example.demo.model.Building;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,53 +18,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BuildingController {
-  final File dbFile = new File("building.xml");
+  final XmlDatabase<Building> buildingDb = new XmlDatabase<>("building.xml", Building.class);
 
   @GetMapping(value = "/api/building/{id}", produces = "application/json")
   public Optional<Building> oneBuilding(@PathVariable String id) throws IOException {
-    var xmlMapper = new XmlMapper();
-
-    List<Building> dataBase;
-
-    dataBase = xmlMapper.readValue(dbFile, new TypeReference<List<Building>>() {
-    });
-
-    return dataBase.stream().filter(b -> b.getId().equals(id)).findFirst();
+    return buildingDb.findFirst(UUID.fromString(id));
   }
 
   @GetMapping(value = "/api/building", produces = "application/json")
   public List<Building> allBuildings() throws IOException {
-    var xmlMapper = new XmlMapper();
-
-    return xmlMapper.readValue(dbFile, new TypeReference<List<Building>>() {
-    });
+    return buildingDb.findAll();
   }
 
   @PostMapping(value = "/api/building", produces = "application/json")
-  public Building addBuilding(@RequestBody Building newBuilding) throws IOException {
+  public Building addBuilding(@RequestBody Building newBuilding, BindingResult bindingResult) throws IOException {
+    Building result = null;
 
-    var xmlMapper = new XmlMapper();
+    if (!bindingResult.hasErrors()) {
+      result = buildingDb.save(newBuilding);
+    }
 
-    List<Building> dataBase = xmlMapper.readValue(dbFile, new TypeReference<List<Building>>() {
-    });
-
-    dataBase.add(newBuilding);
-
-    xmlMapper.writeValue(dbFile, dataBase);
-
-    return newBuilding;
+    return result;
   }
 
   @DeleteMapping(value = "/api/building/{id}", produces = "application/json")
   public void deleteBuilding(@PathVariable String id) throws IOException {
-
-    var xmlMapper = new XmlMapper();
-
-    List<Building> dataBase = xmlMapper.readValue(dbFile, new TypeReference<List<Building>>() {
-    });
-
-    dataBase.removeIf(b -> b.getId().equals(id));
-
-    xmlMapper.writeValue(dbFile, dataBase);
+    buildingDb.delete(UUID.fromString(id));
   }
 }
